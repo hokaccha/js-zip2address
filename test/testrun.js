1e1d5cd882e3e4125d059a6b3047a6a011359cc0
hokaccha <k.hokamura@gmail.com>
2010-11-10 17:11:03 +0900

    API callして住所とってくるところまで実装

--- test/testrun.js
+++ test/testrun.js
@@ -1,5 +1,7 @@
-test("basic", function() {
-	zip2address(1500001, function(address) {
+asyncTest("basic", function() {
+	expect(3);
+	zip2address('150-0001', function(address) {
+		start();
 		equal(address.all, '東京都渋谷区神宮前');
 		equal(address.pref, '東京都');
 		equal(address.city, '渋谷区神宮前');
---
+++ zip2address.js
@@ -0,0 +1,58 @@
+(function(window) {
+
+var d = document,
+	api_url = 'http://www.google.com/transliterate?langpair=ja-Hira|ja';
+	prefs = [
+		'北海道', '青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県',
+		'茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県',
+		'新潟県', '富山県', '石川県', '福井県', '山梨県', '長野県', '岐阜県',
+		'静岡県', '愛知県', '三重県', '滋賀県', '京都府', '大阪府', '兵庫県',
+		'奈良県', '和歌山県', '鳥取県', '島根県', '岡山県', '広島県', '山口県',
+		'徳島県', '香川県', '愛媛県', '高知県', '福岡県', '佐賀県', '長崎県',
+		'熊本県', '大分県', '宮崎県', '鹿児島県', '沖縄県'
+	];
+
+var zip2address = function(zip, callback) {
+	var jsonp_callback = 'zip2address_jsonp' + (new Date()).getTime(),
+		url = api_url + '&jsonp=' + jsonp_callback,
+		head = d.getElementsByTagName('head')[0],
+		script = d.createElement('script');
+
+	// jsonp callback function
+	window[ jsonp_callback ] = function(data) {
+		var address = {};
+		address.all = data[0][1][0];
+
+		// check match pref
+		for (var i = 0, l = prefs.length; i < l; i++) {
+			var pref = prefs[i];
+			if (address.all.indexOf(pref) === 0) {
+				address.pref = pref;
+				address.city = address.all.replace(pref, '');
+				break;
+			}
+		}
+
+		// no match address
+		if (!address.pref && !address.city) {
+			address = undefined;
+		}
+
+		// callback function
+		callback(address);
+
+		// cleaning
+		delete window[ jsonp_callback ];
+		head.removeChild(script);
+	};
+
+	// call api by jsonp
+	url += '&text=' + encodeURIComponent(zip);
+	script.setAttribute('src', url);
+	head.appendChild(script);
+};
+
+// export function
+window.zip2address = zip2address;
+
+})(window);
